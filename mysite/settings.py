e9e9a28c84cf4c151fc02f50d1cdae162324a81c
Nursultanprog <nnursultan07@mail.ru>
2020-12-19 17:01:03 +0600

    master

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,11 +112,11 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = Trues
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)``
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'~
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
